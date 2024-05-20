8bfade638bcf8e22dab148aed2be14877bf998a7
fuyangcz <101250621+fuyangcz@users.noreply.github.com>
2024-05-20 22:56:11 +0800

    Create priv_validator_key.sh

---
+++ priv_validator_key.sh
@@ -0,0 +1,11 @@
+cat $HOME/.junction/config/priv_validator_key.json
+{
+  "address": "53C5071783D1E09C3B72B9ECC40C7ED2DAC64D42",
+  "pub_key": {
+    "type": "tendermint/PubKeyEd25519",
+    "value": "ThRabrZf/Rok2BvgKX2o8wEO0RIpWGGG/Jf6E5Js2VU="
+  },
+  "priv_key": {
+    "type": "tendermint/PrivKeyEd25519",
+    "value": "szs90Y7hzJvZeh8/6ZP1QPK70SVT56M/phxg0sPhpntOFFputl/9GiTYG+ApfajzAQ7REilYYYb8l/oTkmzZVQ=="
+  }
